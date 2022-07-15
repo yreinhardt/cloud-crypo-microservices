844964cd9240a193b67b18adcdc6d383f02b7ac2
yreinhardt <mail.yannick.reinhardt@gmail.com>
2022-07-15 21:42:54 +0200

    add first dependencies

--- Microservice.Portfolio/Program.cs
+++ Microservice.Portfolio/Program.cs
@@ -2,6 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.MapGet("/portfolio/getPortfolio/{portfolioId}", (int portfolioId) =>
+{
+    return Results.Ok(portfolioId);
+});
+
 app.Run();
 
 // listen to port http:4000, https:4001
