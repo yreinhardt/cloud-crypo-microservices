20567d9294c33f283f56c8434bda8f3327afd7d0
yreinhardt <mail.yannick.reinhardt@gmail.com>
2022-07-31 10:32:59 +0200

    edit addAsset endpoint and add deleteAsset endpoint

--- Microservice.Portfolio/Program.cs
+++ Microservice.Portfolio/Program.cs
@@ -14,11 +14,6 @@ builder.Services.AddDbContext<PortfolioDbContext>(
 
 var app = builder.Build();
 
-/*
-PUT: /portfolio/addCoin
-PUT: /portfolio/deleteCoin
- */
-
 // create portfolio
 app.MapPost("/portfolio/createPortfolio", async (Portfolio folio, PortfolioDbContext db) =>
 {
@@ -67,7 +62,7 @@ app.MapDelete("/portfolio/deletePortfolio/{portfolioId}", async (Guid portfolioI
 
 });
 
-// add assets from portfolio with id, request body = folioUpdate (only id and assets needed)
+// add asset from portfolio with id, request body = folioUpdate (only id and assets needed)
 app.MapPut("/portfolio/addAsset", async (Portfolio folioUpdate, PortfolioDbContext db) =>
 {
 
@@ -75,13 +70,13 @@ app.MapPut("/portfolio/addAsset", async (Portfolio folioUpdate, PortfolioDbConte
 
     if (record is null)
     {
-        return Results.NotFound("Provide valid Portfolio Id.");
+        return Results.NotFound("Provide valid Portfolio Identifier.");
     }
 
  
     // check if asset is already in portfolio
     // at this moment possible to add only one asset
-    // TODO fix multiple assets
+    // TODO fix for multiple assets
     var match = record.Assets
         .Where(x => x.Contains(folioUpdate.Assets[0]));
 
@@ -97,9 +92,44 @@ app.MapPut("/portfolio/addAsset", async (Portfolio folioUpdate, PortfolioDbConte
     }
 
     return Results.BadRequest("Portfolio contains Asset already");
+});
 
+// delete asset from portfolio based on id
+app.MapPut("/portfolio/deleteAsset", async (Portfolio folioUpdate, PortfolioDbContext db) =>
+{
+    var record = await db.Portfolio.FindAsync(folioUpdate.PortfolioId);
 
+    if (record is null)
+    {
+        return Results.NotFound("Provide valid Portfolio Identifier.");
+    }
 
+    // at this moment possible to delete only one asset
+    // TODO fix for multiple assets
+
+    // find index of asset portfolio
+    int indexOfValue = record.Assets.FindIndex(x => x.Contains(folioUpdate.Assets[0]));
+
+    if (indexOfValue == -1)
+    {
+        return Results.NotFound("Asset not found");
+    }
+
+    // copy list and update original list from db
+    List<string> updateList = record.Assets;
+
+    // remove purchase price
+    updateList.RemoveAt(indexOfValue + 1);
+
+    // remove asset name
+    updateList.RemoveAt(indexOfValue);
+    
+    record.Assets = updateList;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+    
 });
 
 
