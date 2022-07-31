93e93f7a0d58d7bc2a2da300edca3e74ebba86bf
yreinhardt <mail.yannick.reinhardt@gmail.com>
2022-07-31 16:48:40 +0200

    add support to delete and add multiple assets

--- Microservice.Portfolio/Program.cs
+++ Microservice.Portfolio/Program.cs
@@ -17,10 +17,10 @@ var app = builder.Build();
 // create portfolio
 app.MapPost("/portfolio/createPortfolio", async (Portfolio folio, PortfolioDbContext db) =>
 {
-    
-    if (folio.Assets.Count % 2 != 0)
+    // validate req body => must contain 3 elements (name, price, count)
+    if (folio.Assets.Count % 3 != 0)
     {
-        return Results.BadRequest("Provide correct Assetinformation. Each Asset must have a Name and Price");
+        return Results.BadRequest("Provide correct Assetinformation. Each Asset must have a Name, Price and Count");
     }
 
     folio.CreatedOn = folio.CreatedOn.ToUniversalTime();
@@ -68,30 +68,35 @@ app.MapPut("/portfolio/addAsset", async (Portfolio folioUpdate, PortfolioDbConte
 
     var record = await db.Portfolio.FindAsync(folioUpdate.PortfolioId);
 
+    // validation if records are empty
     if (record is null)
     {
         return Results.NotFound("Provide valid Portfolio Identifier.");
     }
 
- 
-    // check if asset is already in portfolio
-    // at this moment possible to add only one asset
-    // TODO fix for multiple assets
-    var match = record.Assets
-        .Where(x => x.Contains(folioUpdate.Assets[0]));
-
-    if (match != null)
+    // validate req body => must contain 3 elements (name, price, count)
+    if (folioUpdate.Assets.Count % 3 != 0)
     {
-        // asset name
-        record.Assets.Add(folioUpdate.Assets[0]);
-        await db.SaveChangesAsync();
-        // asset price
-        record.Assets.Add(folioUpdate.Assets[1]);
-        await db.SaveChangesAsync();
-        return Results.NoContent();
+        return Results.BadRequest("Provide correct Assetinformation. Each Asset must have a Name, Price and Count");
     }
 
-    return Results.BadRequest("Portfolio contains Asset already");
+    // get asset names from list to check for duplicates
+    // every third element equals asset name
+    var assetNames = folioUpdate.Assets.Where((x, i) => i % 3 == 0).ToList();
+
+    List<string> duplicates = record.Assets.Intersect(assetNames).ToList();
+    // validation check for duplicates
+    if (duplicates.Any())
+    {
+        return Results.BadRequest("Can not add already existing Asset");
+    }
+
+    record.Assets.AddRange(folioUpdate.Assets);
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+
 });
 
 // delete asset from portfolio based on id
@@ -104,8 +109,37 @@ app.MapPut("/portfolio/deleteAsset", async (Portfolio folioUpdate, PortfolioDbCo
         return Results.NotFound("Provide valid Portfolio Identifier.");
     }
 
-    // at this moment possible to delete only one asset
-    // TODO fix for multiple assets
+    // check if multiple assets need to be deleted
+    if (folioUpdate.Assets.Count > 0)
+    {
+        // get indices of assets to delete
+        List<int> indices = new List<int>();
+
+        for (int i = 0; i < folioUpdate.Assets.Count; i++)
+        {
+            // asset name
+            indices.Add(record.Assets.IndexOf(folioUpdate.Assets[i]));
+            // asset price
+            indices.Add(record.Assets.IndexOf(folioUpdate.Assets[i])+1);
+            // number of purchased assets (count)
+            indices.Add(record.Assets.IndexOf(folioUpdate.Assets[i])+2);
+        }
+
+        var recordCopy = record.Assets;
+
+        // remove asset by given index. remove from last to first elements to avoid changing indices
+        for (int i = indices.Count - 1; i >= 0; i--)
+        {
+            recordCopy.RemoveAt(i);
+        }
+
+        record.Assets = recordCopy;
+
+        await db.SaveChangesAsync();
+
+        return Results.NoContent();
+    }
+
 
     // find index of asset portfolio
     int indexOfValue = record.Assets.FindIndex(x => x.Contains(folioUpdate.Assets[0]));
